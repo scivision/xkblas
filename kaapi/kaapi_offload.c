9e2fd60a956e9089614de81c4d034545731de232
Pierre-Etienne Polet <pierre-etienne.polet@inria.fr>
2024-06-26 14:29:25 +0000

    fix cpuset issue

--- kaapi/kaapi_offload.c
+++ kaapi/kaapi_offload.c
@@ -255,7 +255,6 @@ kaapi_offload_config_devices(kaapi_driver_t* driver)
 
   cpu_set_t save_schedset;
   pthread_getaffinity_np(pthread_self(), sizeof(cpu_set_t), &save_schedset);
-
   for (i= 0; i < n_devices; i++)
   {
     /* */
@@ -300,6 +299,7 @@ kaapi_offload_config_devices(kaapi_driver_t* driver)
     kaapi_assert(err ==0);
     kaapi_offload_num_devices++;
   }
+  pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &save_schedset);
 
 out:
   KAAPI_OFFLOAD_INIT_TRACE_OUT
