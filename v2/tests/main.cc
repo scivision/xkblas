143d590f9847020932a5c9bcd8aa7283750799bd
Romain Pereira <romain.pereira@inria.fr>
2024-07-17 16:03:57 +0200

    impossible de bosser sur lille, ça lag trop

--- v2/tests/main.cc
+++ v2/tests/main.cc
@@ -14,14 +14,21 @@ main(void)
     int M = 128;
     int N = M;
     int K = M;
-    int LDA = M;
-    int LDB = M;
-    int LDC = M;
+    int LDA = 3*M;
+    int LDB = 3*M;
+    int LDC = 3*M;
     double _Complex alpha = 1.0;
     double _Complex beta  = 1.0;
+
+    # if 0
     const double _Complex * A = (const double _Complex *) malloc(sizeof(double _Complex) * LDA * LDA);
     const double _Complex * B = (const double _Complex *) malloc(sizeof(double _Complex) * LDB * LDB);
           double _Complex * C = (      double _Complex *) malloc(sizeof(double _Complex) * LDC * LDC);
+    # else
+    const double _Complex * A = (const double _Complex *) 0;
+    const double _Complex * B = (const double _Complex *) (A + LDA * LDA);
+          double _Complex * C = (      double _Complex *) (B + LDB * LDB);
+    # endif
     xkblas_zgemm_async(transA, transB, M, N, K, &alpha, A, LDA, B, LDB, &beta, C, LDC);
 
     xkblas_sync();
