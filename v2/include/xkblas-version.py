179a93432acce3be8e9af62d1031cf409f87afb2
Romain Pereira <romain.pereira@inria.fr>
2024-10-04 15:59:45 +0200

    githash define generator

--- v2/include/xkblas-version.py
+++ v2/include/xkblas-version.py
@@ -14,7 +14,7 @@ def cmd(s):
     print(s)
     return os.system(s)
 
-cmd('sed "s/£GIT_HASH/$(git describe --abbrev=4 --dirty --always --tags)/g" {}/{}-template.h > {}/{}-tmp.h'.format(dstdir, dstfile, dstdir, dstfile))
+cmd('sed "s/£GIT_HASH/$(git describe --abbrev=4 --dirty --always --tags)/g" {}/{}.template > {}/{}-tmp.h'.format(dstdir, dstfile, dstdir, dstfile))
 
 if cmd("test -f {}/{}.h".format(dstdir, dstfile)) or cmd("diff {}/{}.h {}/{}-tmp.h".format(dstdir, dstfile, dstdir, dstfile)):
     cmd("cp {}/{}-tmp.h {}/{}.h".format(dstdir, dstfile, dstdir, dstfile))
