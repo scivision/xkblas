a1361df50d5af2a8587a09e13625d7ca66c5af2e
Romain PEREIRA <rpereira@anl.gov>
2025-02-09 18:00:09 +0000

    merged

--- projects/xkrt/include/xkrt/xkrt-version.h
+++ projects/xkrt/include/xkrt/xkrt-version.h
@@ -1,6 +1,6 @@
 #ifndef __XKRT_VERSION_H__
 # define __XKRT_VERSION_H__
 
-# define XKRT_VERSION_GITHASH "v0.5.0-346-gd09048e-dirty"
+# define XKRT_VERSION_GITHASH "v0.5.0-347-gd81a513-dirty"
 
 #endif /* __XKRT_VERSION_H__ */
