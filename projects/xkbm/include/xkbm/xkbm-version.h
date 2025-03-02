0a9df673a14b939ae1a005b2ab1d38012bb2888f
Romain PEREIRA <rpereira@anl.gov>
2025-03-02 07:02:34 +0000

    removed old thread impl

--- projects/xkbm/include/xkbm/xkbm-version.h
+++ projects/xkbm/include/xkbm/xkbm-version.h
@@ -1,6 +1,6 @@
 #ifndef __XKBM_VERSION_H__
 # define __XKBM_VERSION_H__
 
-# define XKBM_VERSION_GITHASH "v0.5.0-412-gb3ee780-dirty"
+# define XKBM_VERSION_GITHASH "v0.5.0-414-ga59c976-dirty"
 
 #endif /* __XKBM_VERSION_H__ */
