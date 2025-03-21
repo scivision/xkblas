aa6de691f10f5aa13b5e7715266e3b73c7051b8a
Romain PEREIRA <rpereira@anl.gov>
2025-03-21 18:24:01 -0500

    [wip] ze benchmark for latency on lists

--- projects/xkbm/src/ze/benchmarks.cc
+++ projects/xkbm/src/ze/benchmarks.cc
@@ -24,35 +24,114 @@
 # include <xkrt/logger/logger-ze.h>
 # include <xkrt/logger/metric.h>
 
-static ze_driver_handle_t driverHandle;
-static ze_device_handle_t deviceHandle;
-static ze_context_handle_t contextHandle;
+static ze_driver_handle_t driver;
+static ze_device_handle_t device;
+static ze_context_handle_t context;
 
 /////////////////////////////////////
 // LATENCY OF SUBMISSION IN QUEUES //
 /////////////////////////////////////
 
+static void
+cmdlist_immediate_latency_run(benchmark_node_t * node)
+{
+    const int ordinal = 0;
+    const int index   = 0;
 
+    // command list
+    const queue_desc_t queue_desc = {
+        .stype      = ZE_STRUCTURE_TYPE_COMMAND_QUEUE_DESC,
+        .pNext      = NULL,
+        .ordinal    = ordinal,
+        .index      = index,
+        .flags      = ZE_COMMAND_QUEUE_FLAG_EXPLICIT_ONLY,
+        .mode       = ZE_COMMAND_QUEUE_MODE_ASYNCHRONOUS,
+        .priority   = ZE_COMMAND_QUEUE_PRIORITY_PRIORITY_LOW
+    };
+    ze_command_list_t list;
+    ZE_SAFE_CALL(
+        zeCommandListCreateImmediate(
+            context,
+            device,
+           &queue_desc,
+           &list
+        )
+    );
+
+    // event pool
+    const ze_event_pool_desc_t ze_event_pool_desc = {
+        .stype  = ZE_STRUCTURE_TYPE_EVENT_POOL_DESC,
+        .pNext  = NULL,
+        .flags  = ZE_EVENT_POOL_FLAG_HOST_VISIBLE,
+        .count  = 1
+    };
+    ze_event_pool_handle_t pool;
+    ZE_SAFE_CALL(zeEventPoolCreate(context, &pool_desc, 1, &device, &pool));
+
+    // event
+    ze_event_desc_t event_desc = {
+        .stype  = ZE_STRUCTURE_TYPE_EVENT_DESC,
+        .signal = ZE_EVENT_SCOPE_FLAG_HOST,
+        .wait   = ZE_EVENT_SCOPE_FLAG_HOST
+    };
+    ze_event_handle_t event;
+    ZE_SAFE_CALL(zeEventCreate(pool, &desc, &event));
+
+    // allocate memory for latency test
+    const size_t size = 1;
+    const void * src = calloc(1, size);
+
+    const ze_device_mem_alloc_desc_t device_desc = {
+        .stype = ZE_STRUCTURE_TYPE_DEVICE_MEMORY_PROPERTIES,
+        .pNext = NULL,
+        .flags = 0,
+        .ordinal = 0
+    };
+    const size_t alignment = 4 * sizeof(double);
+    void * dst = NULL;
+    ZE_SAFE_CALL(zeMemAllocDevice(context, &device_desc, size, alignment, device, &dst));
+    assert(dst);
+    ZE_SAFE_CALL(zeContextMakeMemoryResident(context, device, dst, size));
+
+    // run the test
+    for (...)
+    {
+        ZE_SAFE_CALL(zeCommandListAppendMemoryCopy(list, dst, src, size, event, n_wait_events, wait_events));
+        ZE_SAFE_CALL(zeCommandListHostSynchronize(list, UINT64_MAX));
+
+    }
+
+    // release stuff
+    ZE_SAFE_CALL(zeEventPoolDestroy(pool));
+    ZE_SAFE_CALL(zeCommandListDestroy(list));
+
+    free(src);
+}
+
+static benchmark_node_t cmdlist_immediate_latency = {
+    .name = "CMDLIST-IMMEDIATE",
+    .desc = "Latency of submitting a H2D copy to an immediate command list",
+    .run = cmdlist_immediate_latency_run
+};
 
 ///////////////////
 // ZE BENCHMARKS //
 ///////////////////
 
-static benchmark_node_t ze_benchmarks = {
+static benchmark_node_t ze = {
     .name = "ze",
     .desc = "Metrics on ZE-supported devices",
-    .parent = NULL,
-    .children = { NULL },
-    .nchildren = 0,
-    .run = NULL,
+    .run = check_devices,
     .enabled = 1
 };
 
 void
 ze_benchmark_push(benchmark_node_t * parent)
 {
-    benchmark_push_children(parent, &ze_benchmarks);
-    // benchmark_push_children(&ze_benchmarks, &ze_benchmarks_h2d);
+    benchmark_push_children(parent, &ze);
+
+    # define LINK(X, Y) benchmark_push_children(&X, &Y)
+    LINK(ze, cmdlist);
 }
 
 void
@@ -62,17 +141,17 @@ ze_benchmark_init(void)
     ZE_SAFE_CALL(zeInit(initFlags));
 
     uint32_t driverCount = 1;
-    ZE_SAFE_CALL(zeDriverGet(&driverCount, &driverHandle));
+    ZE_SAFE_CALL(zeDriverGet(&driverCount, &driver));
 
     uint32_t deviceCount = 1;
-    ZE_SAFE_CALL(zeDeviceGet(driverHandle, &deviceCount, &deviceHandle));
+    ZE_SAFE_CALL(zeDeviceGet(driver, &deviceCount, &device));
 
     ze_context_desc_t contextDesc = {ZE_STRUCTURE_TYPE_CONTEXT_DESC};
-    ZE_SAFE_CALL(zeContextCreate(driverHandle, &contextDesc, &contextHandle));
+    ZE_SAFE_CALL(zeContextCreate(driver, &contextDesc, &context));
 }
 
 void
 ze_benchmark_deinit(void)
 {
-    ZE_SAFE_CALL(zeContextDestroy(contextHandle));
+    ZE_SAFE_CALL(zeContextDestroy(context));
 }
