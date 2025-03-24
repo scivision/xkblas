9146bd40fffd6be4dc25ebec8198bac38cf6c997
Romain PEREIRA <rpereira@anl.gov>
2025-03-24 22:25:39 +0000

    removed zeblas unused files added missing symbols for xkblas_register_memory functions

--- projects/xkblas/src/legacy.cc
+++ projects/xkblas/src/legacy.cc
@@ -5,7 +5,7 @@
 /*   Author: Romain PEREIRA <romain.pereira@inria.fr>              .'* *.'    */
 /*                                                              __/_*_*(_     */
 /*   Created: 2024/12/17 13:03:45 by Romain PEREIRA            / _______ \    */
-/*   Updated: 2025/03/17 22:36:24 by Romain PEREIRA            \_)     (_/    */
+/*   Updated: 2025/03/24 21:59:49 by Romain PEREIRA            \_)     (_/    */
 /*                                                                            */
 /*   License: CeCILL-C                                                        */
 /*                                                                            */
@@ -29,12 +29,14 @@ xkblas_set_modemath(xkblas_mode_math_t mode)
 
 extern "C"
 int
-xkblas_get_ngpus(int * count)
+xkblas_get_ngpus(void)
 {
     xkblas_context_t * context = xkblas_context_get();
     assert(context);
 
-    return xkrt_get_ndevices(&(context->runtime), count);
+    int count = 0;
+    xkrt_get_ndevices(&(context->runtime), &count);
+    return count;
 }
 
 extern "C"
--- projects/xkbm/include/xkbm/xkbm-version.h
+++ projects/xkbm/include/xkbm/xkbm-version.h
@@ -1,6 +1,6 @@
 #ifndef __XKBM_VERSION_H__
 # define __XKBM_VERSION_H__
 
-# define XKBM_VERSION_GITHASH "v0.5.0-455-g5cb795d-dirty"
+# define XKBM_VERSION_GITHASH "v0.5.0-490-g9a7061e-dirty"
 
 #endif /* __XKBM_VERSION_H__ */
