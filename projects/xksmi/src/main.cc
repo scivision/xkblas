15e91e573b1689a6c8d6bea283a8895cf864bc39
Romain PEREIRA <rpereira@anl.gov>
2025-05-05 12:28:29 -0500

    organized xksmi code

--- projects/xksmi/src/main.cc
+++ projects/xksmi/src/main.cc
@@ -12,14 +12,90 @@ print_usage(const char * prog_name)
     LOGGER_INFO("Options:");
     LOGGER_INFO("  -f fps        Frequency to update");
     LOGGER_INFO("  -h            Show this help message");
+    LOGGER_INFO("XKSMI is built on top of XKRT - set `XKRT_HELP=1` to see available environment variables.");
 }
 
 // separators for rendering
-static const char * sep0 = "                                                                                                                                                                                                                                                                                                ";
-static const char * sep1 = "##########################################################################################################################################################################################################################################################################################################################";
-static const char * sep2 = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+static const char * sep_space = "                                                                                                                                                                                                                                                                                                ";
+static const char * sep_hash = "##########################################################################################################################################################################################################################################################################################################################";
+static const char * sep_line = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
 static const char * title = "XKSMI " XKSMI_VERSION_GITHASH;
 static const int titlelen = strlen(title);
+
+// number of rows and cols of the screen
+static int rows, cols;
+# define printsep_hash() mvprintw(y++, x, "%.*s", cols, sep_hash)
+# define printsep_line() mvprintw(y++, x, "%.*s", cols, sep_line)
+
+// current position on which we are drawing
+static int x, y;
+
+// print a driver
+static void
+print_driver(xkrt_runtime_t * runtime, xkrt_driver_type_t driver_type)
+{
+    xkrt_driver_t * driver = runtime->driver_get((xkrt_driver_type_t)driver_type);
+    if (driver == NULL)
+        continue ;
+
+    printsep_hash();
+
+    // print driver name
+    if (driver->f_get_name)
+        mvprintw(y++, x, "Driver `%s`", driver->f_get_name());
+    else
+        mvprintw(y++, x, "Driver `%d`", driver_type);
+
+    // print all devices
+    if (driver->ndevices_commited)
+    {
+        for (int i = 0 ; i < driver->ndevices_commited ; ++i)
+        {
+            xkrt_device_t * device = driver->devices[i];
+            if (device == NULL)
+                continue ;
+
+            printsep_line();
+            mvprintw(y++, x, "Device %u of global id %u", device->driver_id, device->global_id);
+
+            if (driver->f_memory_device_info)
+            {
+                xkrt_device_memory_info_t meminfo[XKRT_DEVICE_MEMORIES_MAX];
+                int nmemories = 0;
+                driver->f_memory_device_info(device->driver_id, meminfo, &nmemories);
+
+                for (int j = 0 ; j < nmemories ; ++j)
+                {
+                    xkrt_device_memory_info_t * mem = meminfo + j;
+
+                    char memory_used_str[64];
+                    xkrt_metric_byte(memory_used_str, sizeof(memory_used_str), mem->used);
+
+                    char memory_total_str[64];
+                    xkrt_metric_byte(memory_total_str, sizeof(memory_used_str), mem->capacity);
+
+                    mvprintw(
+                        y++, x,
+                        "Memory %d - usage %s/%s",
+                        j, memory_used_str, memory_total_str
+                    );
+                }
+            }
+        }
+    }
+    else
+    {
+        mvprintw(y++, x, "No devices found");
+    }
+}
+
+// print title
+static void
+print_title(void)
+{
+    const int nspaces = (cols - titlelen);
+    mvprintw(y++, x, "%*.s%s", nspaces, sep_space, title);
+}
 
 // main loop, both updating and rendering
 static void
@@ -39,76 +115,25 @@ loop(xkrt_runtime_t * runtime, int target_fps)
 
     while (1)
     {
+        // time to draw this frame
+        const uint64_t t0 = xkrt_get_nanotime();
+
         // clear previous frame
         clear();
 
         // ncurses screen position
-        const int x = 0;
-              int y = 0;
+        x = 0;
+        y = 0;
 
         // get screen width and height
-        int rows, cols;
         getmaxyx(stdscr, rows, cols);
-        # define printsep1() mvprintw(y++, x, "%.*s", cols, sep1)
-        # define printsep2() mvprintw(y++, x, "%.*s", cols, sep2)
 
         // print title
-        const int nspaces = (cols - titlelen);
-        mvprintw(y++, x, "%*.s%s", nspaces, sep0, title);
+        print_title();
 
-        // time to draw this frame
-        const uint64_t t0 = xkrt_get_nanotime();
-
-        // for each driver
+        // print each driver
         for (int driver_type = 0 ; driver_type != XKRT_DRIVER_TYPE_MAX ; ++driver_type)
-        {
-            xkrt_driver_t * driver = runtime->driver_get((xkrt_driver_type_t)driver_type);
-            if (driver == NULL)
-                continue ;
-
-            printsep1();
-
-            // print driver name
-            if (driver->f_get_name)
-                mvprintw(y++, x, "Driver `%s`", driver->f_get_name());
-            else
-                mvprintw(y++, x, "Driver `%d`", driver_type);
-
-            // print all devices
-            for (int i = 0 ; i < driver->ndevices_commited ; ++i)
-            {
-                xkrt_device_t * device = driver->devices[i];
-                if (device == NULL)
-                    continue ;
-
-                printsep2();
-                mvprintw(y++, x, "Device %u of global id %u", device->driver_id, device->global_id);
-
-                if (driver->f_memory_device_info)
-                {
-                    xkrt_device_memory_info_t meminfo[XKRT_DEVICE_MEMORIES_MAX];
-                    int nmemories = 0;
-                    driver->f_memory_device_info(device->driver_id, meminfo, &nmemories);
-
-                    for (int j = 0 ; j < nmemories ; ++j)
-                    {
-                        xkrt_device_memory_info_t * mem = meminfo + j;
-
-                        char memory_used_str[64];
-                        xkrt_metric_byte(memory_used_str, sizeof(memory_used_str), mem->used);
-
-                        char memory_total_str[64];
-                        xkrt_metric_byte(memory_total_str, sizeof(memory_used_str), mem->capacity);
-
-                        mvprintw(
-                            y++, x,
-                            "Memory %d - usage %s/%s",
-                            j, memory_used_str, memory_total_str
-                        );
-                    }
-                }
-            }
-        }
+            print_driver(runtime, (xkrt_driver_type_t) driver_type);
 
         // update frame counter
         ++nframes;
@@ -136,7 +161,6 @@ loop(xkrt_runtime_t * runtime, int target_fps)
             usleep((useconds_t) sleepfor / 1000);
         }
     } /* while (1) */
-
 }
 
 int
