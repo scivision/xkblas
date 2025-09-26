4a00f18b75d8e40ef04d79355995f50f7a89ad56
Romain Pereira <rpereira@anl.gov>
2025-09-26 15:48:08 +0000

    Fixed headers

--- src/conf.cc
+++ src/conf.cc
@@ -1,19 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*   conf.cc                                                      .-*-.       */
-/*                                                              .'* *.'       */
-/*   Created: 2024/07/10 10:59:00 by Romain PEREIRA          __/_*_*(_        */
-/*   Updated: 2025/08/20 20:45:46 by Romain PEREIRA         / _______ \       */
-/*                                                          \_)     (_/       */
-/*   License: CeCILL-C                                                        */
-/*                                                                            */
-/*   Author: Pierre-Etienne POLET <pierre-etienne.polet@inria.fr>             */
-/*   Author: Thierry GAUTIER <thierry.gautier@inrialpes.fr>                   */
-/*   Author: Romain PEREIRA <romain.pereira@inria.fr>                         */
-/*                                                                            */
-/*   Copyright: see AUTHORS                                                   */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+** Copyright 2024,2025 INRIA
+**
+** Contributors :
+** Thierry Gautier, thierry.gautier@inrialpes.fr
+** Romain PEREIRA, romain.pereira@inria.fr + rpereira@anl.gov
+**
+** This software is a computer program whose purpose is to execute
+** blas subroutines on multi-GPUs system.
+**
+** This software is governed by the CeCILL-C license under French law and
+** abiding by the rules of distribution of free software.  You can  use,
+** modify and/ or redistribute the software under the terms of the CeCILL-C
+** license as circulated by CEA, CNRS and INRIA at the following URL
+** "http://www.cecill.info".
+
+** As a counterpart to the access to the source code and  rights to copy,
+** modify and redistribute granted by the license, users are provided only
+** with a limited warranty  and the software's author,  the holder of the
+** economic rights,  and the successive licensors  have only  limited
+** liability.
+
+** In this respect, the user's attention is drawn to the risks associated
+** with loading,  using,  modifying and/or developing or reproducing the
+** software by the user in light of its specific status of free software,
+** that may mean  that it is complicated to manipulate,  and  that  also
+** therefore means  that it is reserved for developers  and  experienced
+** professionals having in-depth computer knowledge. Users are therefore
+** encouraged to load and test the software's suitability as regards their
+** requirements in conditions enabling the security of their systems and/or
+** data to be ensured and,  more generally, to use and operate it in the
+** same conditions as regards security.
+
+** The fact that you are presently reading this means that you have had
+** knowledge of the CeCILL-C license and that you accept its terms.
+**/
 
 # include <xkblas/conf.h>
 # include <xkrt/logger/logger.h>
