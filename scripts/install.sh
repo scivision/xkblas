b48f73840801bf9d2baf915f2516169842083e4d
Romain Pereira <rpereira@anl.gov>
2025-10-28 23:37:00 +0000

    install script updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,17 +14,18 @@ export CXX=clang++
 
 WORK_DIRECTORY="$(pwd)"
 
-XKAAPI_BRANCH=master
+XKAAPI_BRANCH=gh200-unified-memory
 XKBLAS_BRANCH=v2.0
 
-XKAAPI_CMAKE_BUILD_TYPE="Debug"
-XKBLAS_CMAKE_BUILD_TYPE="Debug"
+XKAAPI_CMAKE_BUILD_TYPE="Release"
+XKBLAS_CMAKE_BUILD_TYPE="Release"
 
-CMAKE_XKAAPI_OPTS="-DUSE_CUDA=on"
-CMAKE_XKBLAS_OPTS="-DUSE_CUDA=on -DUSE_CUDABLAS=on "
-CMAKE_XKBLAS_OPTS+=" -DUSE_MKL=on -DUSE_CBLAS=on -DUSE_TESTS=on"
+CMAKE_XKAAPI_OPTS="-DUSE_CUDA=on -DSTRICT=off"
+CMAKE_XKBLAS_OPTS="-DUSE_CUDA=off -DUSE_CUBLAS=on -DSTRICT=off "
+CMAKE_XKBLAS_OPTS+="-DUSE_TESTS=on -DUSE_OPENBLAS=on "
+#CMAKE_XKBLAS_OPTS+=" -DUSE_MKL=on -DUSE_CBLAS=on -DUSE_TESTS=on"
 
-export CMAKE_PREFIX_PATH=$CUDA_PATH
+export CMAKE_PREFIX_PATH=$CUDA_PATH:$CMAKE_PREFIX_PATH
 
 INSTALL_SUFFIX="cuda"
 
@@ -46,11 +47,14 @@ mkdir -p $MODULES_DIRECTORY
 # Install XKAAPI #
 ##################
 
-git clone -b $XKAAPI_BRANCH https://gitlab.inria.fr/xkaapi/dev-v2.git $REPO_DIRECTORY/xkaapi
+git clone https://gitlab.inria.fr/xkaapi/dev-v2.git $REPO_DIRECTORY/xkaapi
 cd $REPO_DIRECTORY/xkaapi
+git pull
+git checkout $XKAAPI_BRANCH
 XKAAPI_HASH=$(git rev-parse HEAD | cut -c 1-12)
 buildir=$REPO_DIRECTORY/xkaapi/build/$XKAAPI_HASH/$INSTALL_NAME
 XKAAPI_INSTALL_DIR=$INSTALL_DIRECTORY/xkaapi/$XKAAPI_HASH/$INSTALL_NAME
+rm -rf $buildir
 mkdir -p $buildir
 cd $buildir
 CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH cmake $CMAKE_XKAAPI_OPTS -DCMAKE_BUILD_TYPE="$XKAAPI_CMAKE_BUILD_TYPE" -DCMAKE_INSTALL_PREFIX="$XKAAPI_INSTALL_DIR" $REPO_DIRECTORY/xkaapi
@@ -60,10 +64,13 @@ make install -j
 # Install XKBlas #
 ##################
 
-git clone -b $XKBLAS_BRANCH https://gitlab.inria.fr/xkblas/dev $REPO_DIRECTORY/xkblas
+git clone https://gitlab.inria.fr/xkblas/dev $REPO_DIRECTORY/xkblas
 cd $REPO_DIRECTORY/xkblas
+git pull
+git checkout $XKBLAS_BRANCH
 XKBLAS_HASH=$(git rev-parse HEAD | cut -c 1-12)
 buildir=$REPO_DIRECTORY/xkblas/build/$XKBLAS_HASH/$INSTALL_NAME
+rm -rf $buildir
 XKBLAS_INSTALL_DIR=$INSTALL_DIRECTORY/xkblas/$XKBLAS_HASH/$INSTALL_NAME
 mkdir -p $buildir
 cd $buildir
@@ -96,10 +103,11 @@ sed -i "s,MY_PREFIX_PATH,$XKBLAS_INSTALL_DIR,g"     $XKBLAS_MODULE_FILE
 
 echo "-------------------------------------------------"
 echo "Success. You may type
-    module use $MODULES_DIRECTORY
-    module load xkaapi/$XKAAPI_HASH/$INSTALL_NAME
-    module load xkblas/$XKBLAS_HASH/$INSTALL_NAME
-
+\`\`\`
+module use $MODULES_DIRECTORY
+module load xkaapi/$XKAAPI_HASH/$INSTALL_NAME
+module load xkblas/$XKBLAS_HASH/$INSTALL_NAME
+\`\`\`
 Then build the following program with
     clang++ main.cc -lxkblas
 "
